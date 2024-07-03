f3b56a225d6871cde347d7b0ce204fae9fcbef2d
aluno <G38T47ss>
2024-07-02 21:08:43 -0400

    revisao

---
+++ revisao/src/components/Sentenceinput/index.cy.tsx
@@ -0,0 +1,10 @@
+import SentenceInput from ".";
+
+describe("<SentenceInput />", () => {
+    it("should keep user text", () => {
+        const sentence = "Fala, galera!";
+        const setSentence = () => {};
+        cy.mount(<SentenceInput  sentence={sentence} setSentence={setSentence} />);
+        cy.get('input').should("have.value", sentence);
+    });
+})
